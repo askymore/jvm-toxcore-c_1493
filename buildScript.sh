d3f218473fda1905c951b54d7ded62baf62c07a0
askymore <askymore@sina.com>
2019-01-24 11:39:01 +0800

    Update buildScript.sh

--- buildScript.sh
+++ buildScript.sh
@@ -3,16 +3,16 @@ export BINTRAY_USER=askymore
 export TRAVIS_BRANCH=master
 
 export TARGET=aarch64-linux-android
-./scripts/build-$TARGET -j$(nproc || sysctl -n hw.ncpu) $GOAL
+sh -c ./scripts/build-$TARGET -j$(nproc || sysctl -n hw.ncpu) $GOAL
 
 export TARGET=arm-linux-androideabi
-./scripts/build-$TARGET -j$(nproc || sysctl -n hw.ncpu) $GOAL
+sh -c ./scripts/build-$TARGET -j$(nproc || sysctl -n hw.ncpu) $GOAL
 
 export TARGET=i686-linux-android
-./scripts/build-$TARGET -j$(nproc || sysctl -n hw.ncpu) $GOAL
+sh -c ./scripts/build-$TARGET -j$(nproc || sysctl -n hw.ncpu) $GOAL
 
 export TARGET=x86_64-linux-android
-./scripts/build-$TARGET -j$(nproc || sysctl -n hw.ncpu) $GOAL
+sh -c ./scripts/build-$TARGET -j$(nproc || sysctl -n hw.ncpu) $GOAL
 
 export TARGET=host
-./scripts/build-$TARGET -j$(nproc || sysctl -n hw.ncpu) $GOAL
+sh -c ./scripts/build-$TARGET -j$(nproc || sysctl -n hw.ncpu) $GOAL
